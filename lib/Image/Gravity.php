fd97a5b43d10bb7c09f37bf30e9e573c8bc1c8f8
lelikg <lelikg@gmail.com>
2019-07-24 12:00:52 +0300

    add watermark properties to token

---
+++ lib/Image/Gravity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wix\Mediaplatform\Image;
+
+class Gravity {
+	const NORTHWEST = "north-west";
+	const NORTH = "north";
+	const NORTHEAST = "north-east";
+	const WEST = "west";
+	const CENTER = "center"; // Default
+	const EAST = "east";
+	const SOUTHWEST = "south-west";
+	const SOUTH = "south";
+	const SOUTHEAST = "south-east";
+}
