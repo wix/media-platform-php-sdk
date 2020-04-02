78ab0fedd3f68471ef0d621fa11e49ccae3fe78a
lelikg <lelikg@gmail.com>
2020-04-02 10:56:13 +0300

    add blur to image token

--- test/Image/Auth/TokenTest.php
+++ test/Image/Auth/TokenTest.php
@@ -36,7 +36,7 @@ class TokenTest extends BaseTest
     	$this->assertEquals('/file/path', $decoded['obj'][0][0]->path);
     	$this->assertEquals('<=200', $decoded['obj'][0][0]->width);
 
-        $this->assertEquals($jwt, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwIiwicGF0aCI6IlwvZmlsZVwvcGF0aCIsIndpZHRoIjoiPD0yMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.YIvfZkNo5RoCKiYJeR-IMjDA3vEQ1ySug24lc47RSDU');
+        $this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwIiwicGF0aCI6IlwvZmlsZVwvcGF0aCIsIndpZHRoIjoiPD0yMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.YIvfZkNo5RoCKiYJeR-IMjDA3vEQ1ySug24lc47RSDU', $jwt);
     }
 
     public function testGenerateOriginalImageToken() {
@@ -49,7 +49,20 @@ class TokenTest extends BaseTest
         $this->assertEquals(array('urn:service:file.download'), $decoded['aud']);
         $this->assertEquals('/file/path', $decoded['obj'][0][0]->path);
 
-        $this->assertEquals($jwt, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZpbGVcL3BhdGgifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.jdcOta9QgXkDql-4JIt4DEV-x3HRVaIk4bKlzxjPHwA');
+        $this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZpbGVcL3BhdGgifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.jdcOta9QgXkDql-4JIt4DEV-x3HRVaIk4bKlzxjPHwA', $jwt);
+    }
+
+    public function testGenerateTokenWithBlurRestriction() {
+	    $jwt = Token::createImageToken("appId", "appSecret", "/file/path", 100, 200, '>=0.5') ;
+
+	    $decoded = (array) JWT::decode($jwt, "appSecret", array("HS256"));
+
+	    $this->assertEquals('urn:app:appId', $decoded['sub']);
+	    $this->assertEquals('urn:app:appId', $decoded['iss']);
+	    $this->assertEquals(array('urn:service:image.operations'), $decoded['aud']);
+	    $this->assertEquals('/file/path', $decoded['obj'][0][0]->path);
+	    $this->assertEquals('>=0.5', $decoded['obj'][0][0]->blur);
+	    $this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwIiwicGF0aCI6IlwvZmlsZVwvcGF0aCIsIndpZHRoIjoiPD0yMDAiLCJibHVyIjoiPj0wLjUifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.rMW3x3PX7CPQwJsod-IVAvmNsCdvFQVsfOQ2SMBYopo', $jwt);
     }
 
     public function testGenerateWatermarkToken() {
@@ -68,6 +81,26 @@ class TokenTest extends BaseTest
         $this->assertEquals('<=300', $decoded['obj'][0][0]->height);
         $this->assertEquals('<=300', $decoded['obj'][0][0]->width);
 
-        $this->assertEquals($jwt, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7InBhdGgiOiJcL3BhdGhcL2ZpbGUuanBnIiwiaGVpZ2h0IjoiPD0zMDAiLCJ3aWR0aCI6Ijw9MzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd2F0ZXJtYXJrc1wvcGF0aFwvZmlsZS5wbmciLCJvcGFjaXR5Ijo0MCwicHJvcG9ydGlvbnMiOjAuMjUsImdyYXZpdHkiOiJub3J0aC1lYXN0In19.vIrmJiLC8pSlCmUbM1zERATW5_zklXZffqpGPUE9sDI');
+        $this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7InBhdGgiOiJcL3BhdGhcL2ZpbGUuanBnIiwiaGVpZ2h0IjoiPD0zMDAiLCJ3aWR0aCI6Ijw9MzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd2F0ZXJtYXJrc1wvcGF0aFwvZmlsZS5wbmciLCJvcGFjaXR5Ijo0MCwicHJvcG9ydGlvbnMiOjAuMjUsImdyYXZpdHkiOiJub3J0aC1lYXN0In19.vIrmJiLC8pSlCmUbM1zERATW5_zklXZffqpGPUE9sDI', $jwt);
     }
+
+	public function testGenerateWatermarkTokenWithBlur() {
+		$watermarkProperties = new WatermarkProperties('/watermarks/path/file.png', 40, 0.25, Gravity::NORTHEAST);
+
+		$jwt = Token::createWatermarkToken("appId", "appSecret", "/path/file.jpg",
+			300, 300, $watermarkProperties, ">=0.5" );
+
+		$decoded = (array) JWT::decode($jwt, "appSecret", array("HS256"));
+
+		$this->assertEquals('urn:app:appId', $decoded['sub']);
+		$this->assertEquals('urn:app:appId', $decoded['iss']);
+		$this->assertEquals(array('urn:service:image.watermark'), $decoded['aud']);
+		$this->assertEquals('/path/file.jpg', $decoded['obj'][0][0]->path);
+		$this->assertEquals('/watermarks/path/file.png', $decoded['wmk']->path);
+		$this->assertEquals('<=300', $decoded['obj'][0][0]->height);
+		$this->assertEquals('<=300', $decoded['obj'][0][0]->width);
+		$this->assertEquals('>=0.5', $decoded['obj'][0][0]->blur);
+
+		$this->assertEquals('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOmFwcElkIiwiaXNzIjoidXJuOmFwcDphcHBJZCIsIm9iaiI6W1t7InBhdGgiOiJcL3BhdGhcL2ZpbGUuanBnIiwiaGVpZ2h0IjoiPD0zMDAiLCJ3aWR0aCI6Ijw9MzAwIiwiYmx1ciI6Ij49MC41In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd2F0ZXJtYXJrc1wvcGF0aFwvZmlsZS5wbmciLCJvcGFjaXR5Ijo0MCwicHJvcG9ydGlvbnMiOjAuMjUsImdyYXZpdHkiOiJub3J0aC1lYXN0In19.VwlRGff91FQbuoQTdTOu8l__rN-mhPZsp0hWgIQ23XQ', $jwt);
+	}
 }
